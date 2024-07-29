aff8a79102d4d3a23af6dcc960f170882c45fb85
Koundu <koundinya.g07@gmail.com>
2024-07-29 10:14:32 +0530

    Added Minor Changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const yogaBtn = document.getElementById('yoga');
 const hiitBtn = document.getElementById('hiit');
 const rideBtn = document.getElementById('ride');
-const joinBtn = document.getElementById('join');
+const joinBtn = document.querySelectorAll('#join');
 var programElement = document.getElementsByClassName('display')[0];
+
 
 hiitBtn.addEventListener('mouseover',()=>imageChange('url("./assets/hoverState1.jpg")'));
 hiitBtn.addEventListener('mouseout',()=>imageChange(''));
@@ -12,5 +13,41 @@ rideBtn.addEventListener('mouseout',()=>imageChange(''));
 
 function imageChange(path){
     programElement.style.backgroundImage = path;
-    console.log(programElement.style.backgroundImage);
 }
+
+yogaBtn.addEventListener('mouseover',function(){
+    if(joinBtn[0].querySelector('p').style.borderBottom == ''){
+        joinBtn[0].querySelector('p').style.borderBottom = '2px solid #C79D5B'
+    }else{
+        joinBtn[0].querySelector('p').style.borderBottom = ''
+    }
+})
+
+yogaBtn.addEventListener('mouseout',function(){
+    joinBtn[0].querySelector('p').style.borderBottom = ''
+})
+
+hiitBtn.addEventListener('mouseover',function(){
+    if(joinBtn[1].querySelector('p').style.borderBottom == ''){
+        joinBtn[1].querySelector('p').style.borderBottom = '2px solid #C79D5B'
+    }else{
+        joinBtn[1].querySelector('p').style.borderBottom = ''
+    }
+})
+
+hiitBtn.addEventListener('mouseout',function(){
+    joinBtn[1].querySelector('p').style.borderBottom = ''
+})
+
+
+rideBtn.addEventListener('mouseover',function(){
+    if(joinBtn[2].querySelector('p').style.borderBottom == ''){
+        joinBtn[2].querySelector('p').style.borderBottom = '2px solid #C79D5B'
+    }else{
+        joinBtn[2].querySelector('p').style.borderBottom = ''
+    }   
+})
+
+rideBtn.addEventListener('mouseout',function(){
+    joinBtn[2].querySelector('p').style.borderBottom = ''
+})
